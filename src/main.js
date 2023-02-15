b2f4bf4f250769e1a3fbd9ca2f1e3fceaab55703
JuanFuent.es <input@juanfuent.es>
2023-02-15 11:32:31 -0600

    Disabled snap :/

--- src/main.js
+++ src/main.js
@@ -9,26 +9,24 @@ import {
 
 gsap.registerPlugin(ScrollTrigger)
 
-
-// let lenis = new Lenis({
-//     duration: .8,
-//     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://w-ww.desmos.com/calculator/brs54l4xou
-//     direction: 'vertical', // vertical, horizontal
-//     gestureDirection: 'vertical', // vertical, horizontal, both
-//     smooth: true,
-//     mouseMultiplier: 1,
-//     smoothTouch: false,
-//     touchMultiplier: 2,
-//     infinite: false
-// })
+let lenis = new Lenis({
+    duration: .8,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://w-ww.desmos.com/calculator/brs54l4xou
+    direction: 'vertical', // vertical, horizontal
+    gestureDirection: 'vertical', // vertical, horizontal, both
+    smooth: true,
+    mouseMultiplier: 1,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false
+})
 
 let object = {
     y: 0
 }
+
 let ySetter = gsap.quickSetter(".title", "y", "px")
-let clamp = gsap.utils.clamp(-60, 60)
-// const setScaleX = gsap.quickSetter(".title", "scaleX")
-// const setScaleY = gsap.quickSetter(".title", "scaleY")
+let clamp = gsap.utils.clamp(-120, 120)
 
 ScrollTrigger.create({
     onUpdate: (self) => {
@@ -40,9 +38,8 @@ ScrollTrigger.create({
                 duration: 1.2,
                 ease: Back.easeOut,
                 overwrite: true,
-                scrub: true,
+                scrub: 1,
                 onUpdate: () => ySetter(object.y)
-                // onUpdate: () => ySetter(object.y)
             })
         }
     }
@@ -59,8 +56,8 @@ sections.forEach((section, i) => {
     const img = section.querySelector("img")
     gsap.fromTo(img, {
         y: () => i ? `${-window.innerHeight * getRatio(section)}px` : "0px",
-        scaleX: 2,
-        scaleY: 2
+        scaleX: 1.5,
+        scaleY: 1.5
     }, {
         y: () => `${window.innerHeight * (1 - getRatio(section))}px`,
         ease: "none",
@@ -71,11 +68,11 @@ sections.forEach((section, i) => {
             trigger: section,
             start: () => i ? "top bottom" : "top top",
             end: "bottom top",
-            // snap: 0.5,
             // snap: {
-            //     ease: "power1.inOut",
-            //     duration: 0.35,
-            //     delay: 0.1,
+            //     delay: .1,
+            //     snapTo: .5,
+            //     ease: "none",
+            //     duration: .45
             // },
             scrub: true,
             invalidateOnRefresh: true // to make it responsive
@@ -86,7 +83,7 @@ sections.forEach((section, i) => {
 
 function animate(time) {
     requestAnimationFrame(animate)
-    // lenis.raf(time)
+    lenis.raf(time)
 }
 
 animate()
